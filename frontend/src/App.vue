ecc6085105344ad2ffa64d0b7952a20cedebf18c
SzyJar <107247457+SzyJar@users.noreply.github.com>
2023-08-14 10:28:40 +0200

    nav bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,13 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="nav-bar">
+    <div class="nav-links">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/product">Products</router-link>
+    </div>
+    <div class="login-link">
+      <router-link to="/login">Login</router-link>
+    </div>
   </nav>
   <router-view/>
 </template>
@@ -12,19 +18,48 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #DBDBDB;
 }
 
-nav {
-  padding: 30px;
+body {
+  --primary-color: #333399;
+  --secondary-color: #2d2d86;
+  background: linear-gradient(to right, var(--primary-color), var(--secondary-color), var(--primary-color));
+}
+
+.nav-bar {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 20px;
+  background-color: transparent;
+  margin-top: 20px;
+}
+
+.nav-links {
+  display: flex;
+  gap: 80px;
+  margin-left: 50px;
+}
+
+.login-link {
+  margin-left: auto;
+  margin-right: 50px;
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: white;
+  text-decoration: none;
+  text-transform: uppercase;
+}
+
+nav a:hover {
+  color: #C7C7FF;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: white;
+  transform: scale(1.2);
 }
 </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -14,6 +14,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/product',
+    name: 'product',
+    component: () => import('../views/Product.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue')
   }
 ]
 
